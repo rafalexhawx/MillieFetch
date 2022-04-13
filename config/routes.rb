f81dc7b37d81abafc3ad8e4ff2974ead1212d7e7
SamuelJeon5 <soulstar67@gmail.com>
2022-04-13 09:33:36 +0000

    Cart added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   root "folders#index"
 
   get "/folders/search", to: "folders#search"
+  get "/folders/cart", to: "folders#cart"
+  get 'folders/add_to_cart/:id', to:'folders#add_to_cart', as: 'add_to_cart'
+  get 'folders/remove_from_cart/:id', to: 'folders#remove_from_cart', as: 'remove_from_cart'
+  
+  
   resources :folders
 end
