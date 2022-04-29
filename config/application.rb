38c157d6ed45b7c8f85ef87eb9af853237834fae
Alex <alexandrerobic312@gmail.com>
2022-04-28 22:34:16 -0600

    Fixed application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
-module TestApp
-  class Application < Rails::Application
+class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     # Configuration for the application, engines, and railties goes here.
@@ -16,19 +14,6 @@ module TestApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     config.filter_parameters += [:otp, :password, :session_hash] #Filter out passwords and OTPs from the log (to obfuscate a bit)
-=======
-module Milliefetch
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
->>>>>>> 018d1e4acc635e72e836cd1c302c20422618f722
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-  end
 end
